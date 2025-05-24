410079838dee5e7dedc3ff2b69b9824ceac05b0d
AdityaAWP <aditwpratama1@gmail.com>
2025-05-25 05:57:29 +0700

    Add login page and UI components with Tailwind CSS integration

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import Auth from "@/components/Auth"
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Auth/>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ components/Auth.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+import mock from '../public/images/jago.jpeg'
+
+import { raleway } from "@/app/fonts"
+
+
+export default function LoginPage() {
+  return (
+    <div className={`min-h-screen flex ${raleway.className} `}>
+      <div className="flex-1 bg-gradient-to-r from-[#9c7766] via-[#724e3c] to-[#5C3D2E] md:flex flex-col justify-evenly items-end hidden">
+        <div className="mx-10">
+            <Image src={mock} width={50} height={50} alt="logo"/>
+            <p className="text-white text-2xl mt-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, saepe.</p>
+        </div>
+        <div className="bg-red-900 items-end text-end mr-9">
+            <Image src={mock} width={200} height={200} alt="logo"/>
+        </div>
+      </div>
+      <div className="flex-1 bg-white p-8 flex items-center justify-center">
+        <div className="w-full max-w-md space-y-6">
+          <div className="text-center space-y-2">
+            <h2 className="text-2xl font-bold text-gray-900">Selamat Datang Kembali</h2>
+            <p className="text-gray-600">Silakan masuk untuk melanjutkan perjalanan sejarah Anda</p>
+          </div>
+
+          <div className="space-y-4">
+          <Button
+            variant="outline"
+            className="w-full h-12 flex items-center justify-center gap-3 border-gray-300 cursor-[url(/jago.cur),_pointer]"
+          >
+            <Image src={mock} width={20} height={20} alt="google-logo"/>
+              Masuk dengan Google
+            </Button>
+
+            <div className="text-center">
+              <p className="text-sm  mb-4">
+                Kami hanya mendukung login melalui Google untuk keamanan dan kemudahan akses.
+              </p>
+
+              <p className="text-sm ">
+                Belum memiliki akun?{" "}
+                <a href="#" className="text-green-600">
+                  Daftar sekarang
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
