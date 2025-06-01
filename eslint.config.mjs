8fc7adabca66f1ab999cc3f1ecca0761ffaa9fcf
AdityaAWP <aditwpratama1@gmail.com>
2025-06-01 21:30:24 +0700

    Small Changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,18 @@
-// eslint.config.mjs
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    files: ["**/*.{ts,tsx}"], // Apply this rule to TypeScript files
+  ...compat.config({
+    extends: ["next"],
     rules: {
-      // Disable the rule that prevents explicit 'any' types
-      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
     },
-  },
+  }),
 ];
 
 export default eslintConfig;
